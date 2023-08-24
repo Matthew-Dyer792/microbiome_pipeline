8e425a4463ac353e24f27aef86e08e0689f1cb2f
matthew <mjd443@mun.ca>
2023-08-24 13:49:07 -0230

    linted and added NCBI_key

--- bin/MetaScope_id.R
+++ bin/MetaScope_id.R
@@ -1,20 +1,30 @@
 #!/usr/bin/env Rscript
-# do MetaScope aligning against target indices
+# do MetaScope id and reassignment
 #
-library("MetaScope")
+library(MetaScope)
+library(magrittr)
 
-args = commandArgs(trailingOnly=TRUE)
+NCBI_key <- "522cc5cd1ca59343ba9f55282d9ecfe6c009"
+options("ENTREZ_KEY" = NCBI_key)
+
+# setup a variable to accept CLI args
+args <- commandArgs(trailingOnly = TRUE)
 
 # test if there is at least one argument: if not, return an error
-if (length(args)<2) {
-  stop("At least three argument must be supplied path/to/bam aligner", call.=FALSE)
+if (length(args) < 2) {
+  stop("two arguments must be supplied path/to/bam aligner", call. = FALSE)
 }
 
 if (file.exists(args[1])) {
-	bam = args[1]
+  bam <- args[1]
 }
 
-type = tools::file_ext(args[1])
+type <- tools::file_ext(args[1])
 
 # Align against target index
-metascope_id(input_file = bam, input_type = type, aligner = args[2], num_species_plot = 0)
+metascope_id(
+  input_file = bam,
+  input_type = type,
+  aligner = args[2],
+  num_species_plot = 0
+)
