1626d5b45470b9c477d0b7faa81b36ddf91adb3d
matthew <mjd443@mun.ca>
2023-09-06 11:01:46 -0230

    added NCBI key parameter

--- bin/MetaScope_id.R
+++ bin/MetaScope_id.R
@@ -4,28 +4,28 @@
 library(MetaScope)
 library(magrittr)
 
-NCBI_key <- "522cc5cd1ca59343ba9f55282d9ecfe6c009"
-options("ENTREZ_KEY" = NCBI_key)
-
 # setup a variable to accept CLI args
 args <- commandArgs(trailingOnly = TRUE)
 
 # test if there is at least one argument: if not, return an error
-if (length(args) < 2) {
+if (length(args) < 3) {
   stop("two arguments must be supplied path/to/bam aligner", call. = FALSE)
 }
 
-if (file.exists(args[1])) {
-  bam <- args[1]
+NCBI_key <- args[1]
+options("ENTREZ_KEY" = NCBI_key)
+
+if (file.exists(args[2])) {
+  bam <- args[2]
 }
 
-type <- tools::file_ext(args[1])
+type <- tools::file_ext(bam)
 
 # Align against target index
 metascope_id(
   input_file = bam,
   input_type = type,
-  aligner = args[2],
+  aligner = args[3],
   num_species_plot = 0
 )
 
